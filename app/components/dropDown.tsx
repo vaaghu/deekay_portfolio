0d373f40b0be32a8b3cf64f3cae84ca3c0dad38b
vaaghu <vignesh021102@gmail.com>
2024-04-22 19:20:21 +0530

    drop down moved to click

--- app/components/dropDown.tsx
+++ app/components/dropDown.tsx
@@ -1,27 +1,35 @@
-import * as React from "react";
 import { initialSectionStyle } from "../assets/styles";
 import { menuIcon } from "../assets/images/icons";
 import Image from "next/image";
 import { worksData } from "../utils";
+import { useState } from "react";
 
 export const DropDown = () => {
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+
   return (
     <div className={initialSectionStyle.navDropDown} title="drop down button">
-      <div className={initialSectionStyle.navDropDownButton}>
+      <div
+        className={initialSectionStyle.navDropDownButton}
+        onClick={() => setIsMenuVisible(true)}>
         <Image src={menuIcon} alt="menu icon" />
         Home
       </div>
-      <div className={initialSectionStyle.navDropDownMenu}>
+      <div
+        className={initialSectionStyle.navDropDownMenu}
+        style={{ display: isMenuVisible ? "flex" : "none" }}>
         {Object.values(worksData).map((work, index) => {
           return (
             <button
               onClick={() => {
                 let target = document.body.querySelector(`#${work.link}`);
-                if (target)
+                if (target) {
+                  setIsMenuVisible(false);
                   target.scrollIntoView({
                     behavior: "smooth",
                     block: "center",
                   });
+                }
               }}
               key={index}
               className={initialSectionStyle.navDropDownMenuLinks}>
